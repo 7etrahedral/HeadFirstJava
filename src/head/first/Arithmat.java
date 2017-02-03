ef9d24ecc088d11464ff7c7250e80b69c63d2364
7etrahedral <tetrahedral@gmail.com>
2017-02-03 10:19:56 +0800

    update Arithmatic.java square.java

--- src/head/first/Arithmat.java
+++ src/head/first/Arithmat.java
@@ -4,4 +4,19 @@ package head.first;
  * Created by wib_ on 2/3/2017.
  */
 public class Arithmat {
+    public static void main(String[] args) {
+        Square persegi;
+        float hasilLuas;
+        float hasilKeliling;
+        float panjang = 4;
+        float lebar = 3;
+
+        persegi = new Square();
+
+        hasilKeliling = persegi.hitungKeliling(panjang, lebar);
+        hasilLuas = persegi.hitungLuas(panjang, lebar);
+
+        System.out.println(hasilKeliling);
+        System.out.println(hasilLuas);
+    }
 }
--- src/head/first/Square.java
+++ src/head/first/Square.java
@@ -4,4 +4,17 @@ package head.first;
  * Created by wib_ on 2/3/2017.
  */
 public class Square {
+    float luas;
+    float keliling;
+
+    public float hitungLuas (float p, float l) {
+        luas = p * l;
+        return luas;
+    }
+
+    public float hitungKeliling (float p, float l) {
+        keliling = 2 * (p *l);
+        return keliling;
+    }
+
 }
